001ffd9b4914e7168fce256d8b7b22b82df0a8fa
Quleat <59722082+Quleat@users.noreply.github.com>
2023-10-22 18:09:28 +0300

    Initial base of the project, the first scheme of classes (But likely not the last)

---
+++ ConverterCppToPython/converterctop.h
@@ -0,0 +1,54 @@
+#ifndef CONVERTERCTOP_H
+#define CONVERTERCTOP_H
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+//Base (interface) class for expressions
+class expressionObj{
+public:
+    virtual void load(string input);
+    virtual string produce();
+};
+
+//Class for translating variables and literals (Doing nothing)
+class obj : public expressionObj {
+    string value = "";
+public:
+    obj();
+    obj(string in);
+    obj(const obj& right);
+    void load(string input);
+    string produce();
+};
+
+//Class for output/input work
+class ioExpression : expressionObj{
+    expressionObj object;
+    string method;
+public:
+    ioExpression();
+    ioExpression(expressionObj o);
+    ioExpression(expressionObj o, string m);
+    void load(string input);
+    void load(expressionObj o, string m);
+    string produce();
+};
+
+//HUGE class for identifying expressions
+class Converter{
+public:
+    static bool isTrash(string in);
+    static bool isIO(string in);
+    static bool isObj(string in);
+
+    static string Convert(string in);
+};
+
+
+
+
+#endif // CONVERTERCTOP_H
